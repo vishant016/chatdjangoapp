a107ecc99d3498ef1fe47809c729d58074b7de7a
vishant016 <77335340+vishant016@users.noreply.github.com>
2021-05-24 21:01:36 +0530

    Update urls.py

--- chat/urls.py
+++ chat/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    # path('',include('chatapp.urls')),
+    path('',include('chatapp.urls')),
     path('admin/', admin.site.urls),
     path('chatapp/',include('chatapp.urls'))
 ]
